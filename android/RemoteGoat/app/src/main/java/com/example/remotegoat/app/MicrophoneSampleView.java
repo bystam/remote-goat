5bb7ceaef214f1b58039c2af94664436b2f7d6de
Jens Arvidsson <jensarv@gmail.com>
2014-05-18 00:41:44 +0200

    kommenterade ut allt

--- android/RemoteGoat/app/src/main/java/com/example/remotegoat/app/MicrophoneSampleView.java
+++ android/RemoteGoat/app/src/main/java/com/example/remotegoat/app/MicrophoneSampleView.java
@@ -24,18 +24,15 @@ import java.util.concurrent.ThreadPoolExecutor;
 /**
  * Created by jensa on 17/05/2014.
  */
-public class MicrophoneSampleView implements SurfaceHolder.Callback {
+public class MicrophoneSampleView {
     int amplitude = 0;
     int xCoord = 0;
     SurfaceView view;
     static boolean animationRunning = false;
-    Animator animator;
 
     //Constructor that is called when inflating a view from XML.
     public MicrophoneSampleView(SurfaceView output){
         view = output;
-        SurfaceHolder holder = output.getHolder();
-        holder.addCallback(this);
     }
 
     public void startAnimation(){
@@ -48,92 +45,91 @@ public class MicrophoneSampleView implements SurfaceHolder.Callback {
         xCoord++;
         Point next = new Point(xCoord + 150, amplitude + 100);
         Point reverse = new Point(xCoord + 150, -amplitude + 100);
-        animator.points.add(new Line(next, reverse));
     }
 
     public void stopAnimation(){
         Log.d("JORR", "Stopping animation");
         animationRunning = false;
         xCoord = 0;
-        animator.clear();
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        tryDrawing(holder);
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int frmt, int w, int h) {
-        tryDrawing(holder);
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {}
-
-    private void tryDrawing(SurfaceHolder holder) {
-        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(2);
-        animator = new Animator(holder);
-        executor.execute(animator);
-    }
-
-    private class Animator implements Runnable {
-        private SurfaceHolder holder;
-        public ConcurrentLinkedQueue<Line> points = new ConcurrentLinkedQueue<Line>();
-
-        public Animator(SurfaceHolder hold) {
-            holder = hold;
-        }
-
-        @Override
-        public void run() {
-            while (true) {
-                Bitmap bitmap =  Bitmap.createBitmap(holder.getSurfaceFrame().width(), holder.getSurfaceFrame().height(), Bitmap.Config.ARGB_8888);
-                Canvas bitmapCanvas = new Canvas(bitmap);
-                drawAnimation(bitmapCanvas);
-                Canvas canvas = holder.lockCanvas();
-                if (canvas == null) {
-                } else {
-                    if (animationRunning) {
-                        Paint p = new Paint ();
-                        p.setColor(Color.BLACK);
-                        canvas.drawBitmap(bitmap, 0,0 , p);
-//                        drawAnimation(canvas);
-                    } else {
-                        }
-                    }
-                    holder.unlockCanvasAndPost(canvas);
-                }
-        }
-
-        private void drawAnimation(Canvas canvas) {
-            canvas.drawARGB(255, 255, 128, 128);
-            Paint color = new Paint();
-            color.setColor(Color.BLACK);
-            color.setStrokeWidth(10);
-            for(Line line : points) {
-                canvas.drawLine(line.start.x, line.start.y, line.end.x, line.end.y, color);
-            }
-        }
-
-        public void clear (){
-            points.clear();
-            Canvas canvas = holder.lockCanvas();
-            canvas.drawARGB(255, 255, 128, 128);
-            holder.unlockCanvasAndPost(canvas);
-//            path.reset();
-        }
-
-    }
-
-    private class Line {
-
-        public Line(Point start, Point end){
-            this.start = start;
-            this.end = end;
-        }
-        public Point start;
-        public Point end;
-    }
+//    @Override
+//    public void surfaceCreated(SurfaceHolder holder) {
+//        tryDrawing(holder);
+//    }
+//
+//    @Override
+//    public void surfaceChanged(SurfaceHolder holder, int frmt, int w, int h) {
+//        tryDrawing(holder);
+//    }
+//
+//    @Override
+//    public void surfaceDestroyed(SurfaceHolder holder) {}
+//
+//    private void tryDrawing(SurfaceHolder holder) {
+//        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(2);
+//        animator = new Animator(holder);
+//        executor.execute(animator);
+//    }
+//
+//    private class Animator implements Runnable {
+//        private SurfaceHolder holder;
+//        public ConcurrentLinkedQueue<Line> points = new ConcurrentLinkedQueue<Line>();
+//
+//        public Animator(SurfaceHolder hold) {
+//            holder = hold;
+//        }
+//
+//        @Override
+//        public void run() {
+//            while (true) {
+//                Bitmap bitmap = Bitmap.createBitmap(holder.getSurfaceFrame().width(), holder.getSurfaceFrame().height(), Bitmap.Config.ARGB_8888);
+//                Canvas bitmapCanvas = new Canvas(bitmap);
+//                drawAnimation(bitmapCanvas);
+//                Canvas canvas = holder.lockCanvas();
+//                if (canvas == null) {
+//                } else {
+////                    if (animationRunning) {
+//                    Paint p = new Paint();
+//                    p.setColor(Color.BLACK);
+//                    canvas.drawBitmap(bitmap, 0, 0, p);
+////                        drawAnimation(canvas);
+////                    } else {
+////                        }
+////                    }
+//                    holder.unlockCanvasAndPost(canvas);
+//                }
+//            }
+//        }
+//
+//        private void drawAnimation(Canvas canvas) {
+//            canvas.drawARGB(255, 255, 128, 128);
+//            Paint color = new Paint();
+//            color.setColor(Color.BLACK);
+//            color.setStrokeWidth(10);
+//            for(Line line : points) {
+//                canvas.drawLine(line.start.x, line.start.y, line.end.x, line.end.y, color);
+//            }
+//        }
+//
+//        public void clear (){
+//            points.clear();
+//            Canvas canvas = holder.lockCanvas();
+//            canvas.drawARGB(255, 255, 128, 128);
+//            holder.unlockCanvasAndPost(canvas);
+////            path.reset();
+//        }
+//
+//    }
+//
+//    private class Line {
+//
+//        public Line(Point start, Point end){
+//            this.start = start;
+//            this.end = end;
+//        }
+//        public Point start;
+//        public Point end;
+//    }
 }
 
