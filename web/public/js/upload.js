58753fd65b8557fddad5903d38da1040f9f50b4e
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-05-17 19:59:12 +0200

    handles some pretty device orientation client side

--- web/public/js/upload.js
+++ web/public/js/upload.js
@@ -1,6 +1,16 @@
 $(function () {
-  console.log('local socket is up?');
   var socket = io.connect('http://localhost');
-  console.log('socket');
   socket.emit ('shake', {});
+
+  if (window.DeviceMotionEvent) {
+    $('#acc').text('motion is supported');
+    window.addEventListener('devicemotion', function(event) {
+      var acc = event.acceleration;
+      $('#x').text(acc.x);
+      $('#y').text(acc.y);
+      $('#z').text(acc.z);
+    });
+  } else {
+    $('#acc').text('acc is supported');
+  }
 });
